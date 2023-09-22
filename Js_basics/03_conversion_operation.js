c60aff1773a7875ca59c7d29dc76cd63ff08bbba
Rhythm Mahale <88775908+Rpm2002@users.noreply.github.com>
2023-09-22 23:11:03 +0000

    lec3 updates

--- Js_basics/03_conversion_operation.js
+++ Js_basics/03_conversion_operation.js
@@ -1,4 +1,28 @@
-let score=33;
+// let score="33";
+// let score="33abc"
+// let score=null;
+// let score=undefined;
 
-console.log(typeof(score));
-console.log(typeof score);
+// console.log(typeof(score));
+// console.log(typeof score);
+
+// let valueinnumber=Number(score);
+// console.log(typeof (valueinnumber));
+// console.log(valueinnumber);
+
+// "33" => 33
+// "33abc"=> NaN
+// true => 1;  false=>0
+
+// let islogged="Rhythm";
+// let islogged="";
+let islogged=1;
+let boolislogged=Boolean(islogged);
+console.log(boolislogged);
+
+/*
+1=> true
+"" => false
+"rhythm"=> true
+
+*/
