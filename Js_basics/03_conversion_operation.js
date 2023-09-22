d731849bf8663ebaae765f02ef76cbae300f16e4
Rhythm Mahale <88775908+Rpm2002@users.noreply.github.com>
2023-09-22 23:57:50 +0000

    done

--- Js_basics/03_conversion_operation.js
+++ Js_basics/03_conversion_operation.js
@@ -18,7 +18,7 @@
 // let islogged="";
 let islogged=1;
 let boolislogged=Boolean(islogged);
-console.log(boolislogged);
+// console.log(boolislogged);
 
 /*
 1=> true
@@ -26,3 +26,34 @@ console.log(boolislogged);
 "rhythm"=> true
 
 */
+
+// ************************************** OPERATIONS ****************************************
+let str1="hello";
+let str2=" world";
+
+let str3=str1+str2;
+
+// console.log(str3);
+
+// console.log(1 + "2");  // 12   These are all ouputs according to the rule of ECMA script
+// console.log("1" + "2");  // 12
+// console.log("1" + 2 + 2);// 122
+// console.log(1 + 2+ "2"); // 32
+
+console.log("2" > 1);  // true
+console.log("02" >1);  // true
+
+console.log(null>0);  //false
+console.log(null==0); //false
+console.log(null>=0); // true
+
+/* The reason is that an equality check == and comparisions < < >= <=  work differntly
+   Comparision converts null to a number, trating it as 0
+   Therefore null>=0 is true and  null>0 is false
+
+*/
+
+console.log(2===2)  // it checks if both datatypes are same or not and does not perform type conversion
+
+
+
