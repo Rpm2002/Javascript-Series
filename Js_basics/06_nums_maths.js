18405053d890daf5742bf398f2ac4818258bfeda
Rhythm Mahale <88775908+Rpm2002@users.noreply.github.com>
2023-10-22 06:25:32 +0000

    numbers and maths in js

---
+++ Js_basics/06_nums_maths.js
@@ -0,0 +1,14 @@
+const score=400
+console.log(score);
+
+const balance= new Number(200);
+console.log(balance);
+
+console.log(balance.toString().length);  // conversion to string and then finding length of string
+console.log(balance.toFixed(2)); // fix the number of decimals 
+
+const othernumber=132.43;
+console.log(othernumber.toPrecision(3));
+
+const hundreds=10000000;
+console.log(hundreds.toLocaleString('en-IN'));
