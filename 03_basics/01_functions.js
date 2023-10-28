fd54ca808c76c33f522186d02a0557c03a49b727
Rhythm Mahale <88775908+Rpm2002@users.noreply.github.com>
2023-10-28 14:43:03 +0000

    Functions intro

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,26 @@
+function name()
+{
+    console.log('R');
+    console.log('H');
+    console.log('Y');
+    console.log('T');
+    console.log('H');
+    console.log('M');
+}
+
+// name();
+
+// function add(a,b)
+// {
+//     console.log(a+b);
+// }
+
+function add(a,b)
+{
+    let result=a+b;
+    return result;
+}
+
+
+const add_result=add(30,5);
+// console.log("Result : ",add_result);
