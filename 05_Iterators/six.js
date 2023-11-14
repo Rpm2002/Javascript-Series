69bad0a9d1c61af7f532ff98e176f5c8b80f3aaa
Rhythm Mahale <88775908+Rpm2002@users.noreply.github.com>
2023-11-14 03:58:50 +0000

    using filter method

---
+++ 05_Iterators/six.js
@@ -0,0 +1,49 @@
+// const coding=['js','cpp','ruby','java']
+
+// const values=coding.forEach( (item)=>{
+//     // console.log(item);
+//     // return item;                 forEach hamesha undefined hi return karega, kuch store nhi karta
+// })
+
+// console.log(values);
+
+const mynums=[1,2,3,4,5,6,7,8,9,10]
+
+// const newnums=mynums.filter( (num)=>num>4)
+
+
+// const newnums=mynums.filter( (num)=>{
+//     return num>4  // {} i.e scope declare kiya hai isliye return lagana hi padega
+// })
+
+// const newnums=[]
+
+// mynums.forEach((num)=>{
+//     if(num>4)
+//     {
+//         newnums.push(num)
+//     }
+// })
+// console.log(newnums);
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+//   const userBooks=books.filter( (bk)=> bk.genre==='Non-Fiction')
+
+    // const userBooks=books.filter( (bk)=> bk.edition>2005)
+
+    let userBooks=books.filter( (bk)=>{
+        return bk.publish>1995 && bk.genre==='History'
+    })
+
+  console.log(userBooks);
