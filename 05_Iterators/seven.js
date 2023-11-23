ccc17c89d9e80ccc8bfec78781b16a30e06f940c
Rhythm Mahale <88775908+Rpm2002@users.noreply.github.com>
2023-11-23 04:08:43 +0000

    difference between map and filter

--- 05_Iterators/seven.js
+++ 05_Iterators/seven.js
@@ -16,3 +16,15 @@ console.log(newNums);
 // It returns a new array made up of the return values from the callback function.
 // The original array does not get altered, and the returned array may contain different 
 // elements than the original array.
+
+// ******** Difference beyween map and filter
+
+// They both return a new array.
+// map returns a new array of elements where you have applied some function on the element
+// so that it changes the original element (typically). filter returns a new array of the elements
+// of the original array (with no change to the original values). filter will only return elements
+// where the function you specify returns a value of true for each element passed to the function.
+
+// So map returns the same number of elements as the original, but the element value will be
+// transformed in some way and filter will return the same or less number of elements than the original
+// but not change the original elements’ values.
